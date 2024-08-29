dd34fcfd4aada756fcbbe35f902285481aed89d1
Caseywebs <caseywebs964@gmail.com>
2024-08-29 21:10:10 +0300

    Update system .js

--- plugins/system .js
+++ plugins/system .js
@@ -1,6 +1,7 @@
 const config = require('../config')
 const {cmd , commands} = require('../command')
 const os = require("os")
+const {} = require('../lib/functions')
 cmd({
     pattern: "system",
     alias: ["status","botinfo"],
@@ -10,9 +11,9 @@ cmd({
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-let status = '*uptime: * ' ${runtime(process.uptime())}
-'*Ram usage:*' ${(progress.memoryusage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${math.round(require('os').totalmem / 1024 / 1024)}MB
-'*Hostname:*' ${os.hostname()}
+let status = '*uptime: *  ${runtime(process.uptime())}
+'*Ram usage:* ${(progress.memoryusage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${math.round(require('os').totalmem / 1024 / 1024)}MB
+'*Hostname:* ${os.hostname()}
 '*owner:* caseyrhodes & caseyweb'
   .
  return reply('${status}')
