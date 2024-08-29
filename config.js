75901893952e1f6e9f2317618ae8d5f72b600d23
Caseywebs <caseywebs964@gmail.com>
2024-08-29 13:13:57 +0300

    Update config.js

--- config.js
+++ config.js
@@ -8,4 +8,5 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID,
 ALIVE_IMG: process.env.ALIVE_IMG || "https://ibb.co/QnmmhLr=4",
 ALIVE_MSG:process.env.ALIVE_MSG || "Hello, i am caseyweb am alive now!!",
+AUTO_READ_STATUS:process.env.AUTO_READ_STATUS || "true",
 };
