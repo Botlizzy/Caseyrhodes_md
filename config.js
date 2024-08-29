3f60ad1674025c4dea267bcf0199fba33fc194ad
Caseywebs <caseywebs964@gmail.com>
2024-08-29 21:15:54 +0300

    Update config.js

--- config.js
+++ config.js
@@ -9,4 +9,5 @@ SESSION_ID: process.env.SESSION_ID,
 ALIVE_IMG: process.env.ALIVE_IMG || "https://ibb.co/QnmmhLr=4",
 ALIVE_MSG:process.env.ALIVE_MSG || "Hello, i am caseyweb am alive now!!",
 AUTO_READ_STATUS:process.env.AUTO_READ_STATUS || "true",
+MODE:process.env.MODE || "public",
 };
